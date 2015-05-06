f95e7fad42065a5467ef926d4ce8fadbc97eb261
Hans Kirchner <hans-kirchner@gmx.de>
2015-05-06 17:48:49 +0200

    Update repos, fix configs

--- external/modules/graphics_test/src/graphics_test.cpp
+++ external/modules/graphics_test/src/graphics_test.cpp
@@ -10,6 +10,8 @@ bool GraphicsTest::initialize() {
 
     config = getConfig();
 
+    logger.debug() << getPriority();
+
     return true;
 }
 
@@ -22,7 +24,11 @@ bool GraphicsTest::cycle() {
     int height = config->get<int>("height");
 
     imagePtr->resize(width, height, lms::imaging::Format::BGRA);
-    imagePtr->fill(255);
+    if(config->get<int>("draw") == 1) {
+        imagePtr->fill(255);
+    } else {
+        imagePtr->fill(0);
+    }
 
     logger.time("predraw");
     lms::imaging::BGRAImageGraphics g(*imagePtr);
@@ -33,23 +39,44 @@ bool GraphicsTest::cycle() {
     g.setColor(lms::imaging::blue);
 
     static int linex = 0, linedelta = 1;
-    if(linex >= 100) {
-        linedelta = -1;
-    } else if(linex < 0) {
-        linedelta = 1;
+
+    if(config->get<int>("draw") == 1) {
+        if(linex >= 100) {
+            linedelta = -1;
+        } else if(linex < 0) {
+            linedelta = 1;
+        }
+        linex += linedelta;
+        g.drawLine(200, 200, linex, (int)sqrt(100*100 - linex * linex));
+
+        static int size = 100, delta = -1;
+        if(size == 0) delta = 1;
+        if(size == 200) delta = -1;
+        size += delta;
+
+        g.setColor(lms::imaging::red);
+        g.drawRect(150, 150, 2*size, size);
+        g.setColor(lms::imaging::yellow);
+        g.drawRect(151, 151, 2*size-2, size-2);
+    } else {
+        if(linex >= 100) {
+            linedelta = -1;
+        } else if(linex < 0) {
+            linedelta = 1;
+        }
+        linex += linedelta;
+        g.drawLine(0, 0, linex, (int)sqrt(100*100 - linex * linex));
+
+        static int size = 100, delta = -1;
+        if(size == 0) delta = 1;
+        if(size == 200) delta = -1;
+        size += delta;
+
+        g.setColor(lms::imaging::red);
+        g.drawRect(50, 50, 2*size, size);
+        g.setColor(lms::imaging::yellow);
+        g.drawRect(51, 51, 2*size-2, size-2);
     }
-    linex += linedelta;
-    g.drawLine(0, 0, linex, (int)sqrt(100*100 - linex * linex));
-
-    static int size = 100, delta = -1;
-    if(size == 0) delta = 1;
-    if(size == 200) delta = -1;
-    size += delta;
-
-    g.setColor(lms::imaging::red);
-    g.drawRect(50, 50, 2*size, size);
-    g.setColor(lms::imaging::yellow);
-    g.drawRect(51, 51, 2*size-2, size-2);
     logger.timeEnd("draw");
 
     return true;
